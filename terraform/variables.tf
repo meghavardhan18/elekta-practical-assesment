46b2b6aae1a0cd7d74b98a73f4f380ac384a280f
Meghavardhan Reddy <meghavardhanreddy@Mac.lan>
2026-04-28 00:09:04 -0500

    formmated teh varible.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,6 +39,6 @@ variable "password" {
   description = "admin_password"
   sensitive   = true
 }
-variable "subscription_id"{
-  type        = string
+variable "subscription_id" {
+  type = string
 }
